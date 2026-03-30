3d930e669f79b9ede6b9a3ccace5502e5166d61d
eoyelade <oyehimar@gmail.com>
2026-03-30 17:11:44 +0100

    feat: implement main game menu and greetings

---
+++ start/welcome.go
@@ -0,0 +1,12 @@
+package main
+
+import(
+	"fmt"
+	"log"
+)
+
+func main(){
+	log.Print("Hello, Go World! Welcome to our mini-game!")
+	fmt.Println("Greetings, Adventurer! Prepare for your quest!")
+	fmt.Println("Press 'S' to start a new game.")
+}
