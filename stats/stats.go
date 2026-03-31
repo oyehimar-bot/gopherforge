bbf8e784a205d18255d1cd01366cd5b01bb0e713
eoyelade <oyehimar@gmail.com>
2026-03-31 15:48:32 +0100

    stats: update player attributes based on game events

--- stats/stats.go
+++ stats/stats.go
@@ -4,11 +4,12 @@ import "fmt"
 
 const (
 	//Enemies Stats
-	maxEnemiesOnScreen = 10
+	maxEnemiesOnScreen   = 10
 	bossHealthMultiplier = 2.5
-	gameVersion = "1.0.0"
+	gameVersion          = "1.0.0"
 )
-func main()  {
+
+func main() {
 	//Player Stats
 	playerName := "Adventurer"
 	playerHealth := 100
@@ -16,12 +17,20 @@ func main()  {
 	playerGold := 123.45
 	isPoisoned := false
 
+	// Dynamic Game Event
+	newPlayerHealth := playerHealth + 25
+	newPlayerGold := playerGold + 10.75
+	newIsPoisoned := true
+
 	fmt.Println("Player:", playerName)
-	fmt.Println("HP:", playerHealth)
-	fmt.Println("MP:", playMana)
-	fmt.Println("GP:", playerGold)
-	fmt.Println("Poisoned:", isPoisoned)
+	fmt.Println("Original HP:", playerHealth)
+	fmt.Println("Original MP:", playMana)
+	fmt.Println("Original GP:", playerGold)
+	fmt.Println("Original poison state:", isPoisoned)
 	fmt.Println("Numbers of enemies on screen:", maxEnemiesOnScreen)
 	fmt.Println("Boss HP is multiplied by", bossHealthMultiplier)
 	fmt.Println("Game version:", gameVersion)
+	fmt.Println("Updated HP:", newPlayerHealth)
+	fmt.Println("Updated GP:", newPlayerGold)
+	fmt.Println("Updated poison state:", newIsPoisoned)
 }
