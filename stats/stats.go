efa089ffc1994e241cc7562414825232cdf8df4b
eoyelade <oyehimar@gmail.com>
2026-03-31 15:08:33 +0100

    feat: implement base player stats logic

---
+++ stats/stats.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main()  {
+	playerName := "Adventurer"
+	playerHealth := 100
+	playMana := 50
+	playerGold := 123.45
+	isPoisoned := false
+
+	fmt.Println("Player:", playerName)
+	fmt.Println("HP:", playerHealth)
+	fmt.Println("MP:", playMana)
+	fmt.Println("GP:", playerGold)
+	fmt.Println("Poisoned:", isPoisoned)
+}
