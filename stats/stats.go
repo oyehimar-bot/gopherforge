30851e3eccb286657c98663f7445f6cd8a038427
eoyelade <oyehimar@gmail.com>
2026-03-31 15:57:49 +0100

    fix: correct typo in playerMana variable and update constant naming to idiomatic Go style

--- stats/stats.go
+++ stats/stats.go
@@ -4,32 +4,32 @@ import "fmt"
 
 const (
 	//Enemies Stats
-	maxEnemiesOnScreen   = 10
-	bossHealthMultiplier = 2.5
-	gameVersion          = "1.0.0"
+	MaxEnemiesOnScreen   = 10
+	BossHealthMultiplier = 2.5
+	GameVersion          = "1.0.0"
 )
 
 func main() {
 	//Player Stats
 	playerName := "Adventurer"
 	playerHealth := 100
-	playMana := 50
+	playerMana := 50
 	playerGold := 123.45
 	isPoisoned := false
 
 	// Dynamic Game Event
-	newPlayerHealth := playerHealth + 25
-	newPlayerGold := playerGold + 10.75
-	newIsPoisoned := true
+	newPlayerHealth := playerHealth + 25 // Player picks up a health potion
+	newPlayerGold := playerGold + 10.75 // Enemy drops gold
+	newIsPoisoned := true // Player enters poisoned area
 
 	fmt.Println("Player:", playerName)
 	fmt.Println("Original HP:", playerHealth)
-	fmt.Println("Original MP:", playMana)
+	fmt.Println("Original MP:", playerMana)
 	fmt.Println("Original GP:", playerGold)
 	fmt.Println("Original poison state:", isPoisoned)
-	fmt.Println("Numbers of enemies on screen:", maxEnemiesOnScreen)
-	fmt.Println("Boss HP is multiplied by", bossHealthMultiplier)
-	fmt.Println("Game version:", gameVersion)
+	fmt.Println("Numbers of enemies on screen:", MaxEnemiesOnScreen)
+	fmt.Println("Boss HP is multiplied by", BossHealthMultiplier)
+	fmt.Println("Game version:", GameVersion)
 	fmt.Println("Updated HP:", newPlayerHealth)
 	fmt.Println("Updated GP:", newPlayerGold)
 	fmt.Println("Updated poison state:", newIsPoisoned)
