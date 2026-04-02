975edfef1ca87545973ceb8a0b16fa35060dcd07
eoyelade <oyehimar@gmail.com>
2026-04-02 16:42:33 +0100

    feat: add nested position struct for player movement

--- models/models.go
+++ models/models.go
@@ -2,16 +2,35 @@ package main
 
 import "fmt"
 
-type Weapon struct{
-	Name string
-	Damage int
+//Weapon character structure
+type Weapon struct {
+	Name      string
+	Damage    int
 	Durabilty float64
 }
-func main()  {
+
+type Positon struct {
+	X, Y float64
+}
+
+type Player struct {
+	Positon Positon // A struct inside a struct
+}
+
+func main() {
+	// Creating an instance using field names (recommended)
 	RustySword := Weapon{
-		Name: "Usang",
-		Damage: 20,
+		Name:      "Usang",
+		Damage:    20,
 		Durabilty: 5.0,
 	}
 	fmt.Println(RustySword.Damage)
+
+	Inyang := Player{
+		Positon: Positon{ 
+			X: 8.7,
+			Y: 3.6,
+		},
+	}
+	fmt.Println(Inyang.Positon.Y) // Access nested fields
 }
