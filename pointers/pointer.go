ddadb1e9befe69f10d26941e413a5e0fd436eafb
eoyelade <oyehimar@gmail.com>
2026-04-08 13:17:28 +0100

    feat: add safety check for nil character pointers

--- pointers/pointer.go
+++ pointers/pointer.go
@@ -3,22 +3,42 @@ package main
 import "fmt"
 
 type Weapon struct {
-	Name string
-	Damage int
+	Name       string
+	Damage     int
 	Durability float64
 }
 
-func Upgrage(w *Weapon)  {
+type Character struct {
+	Name string
+}
+
+func Upgrade(w *Weapon) {
 	w.Damage += 5
 }
 
-func main()  {
+func BoostCharacter(c *Character) {
+	if c == nil {
+		fmt.Println("Character not found")
+	} else {
+		fmt.Println(c.Name)
+	}
+}
+
+func main() {
 	Gun := Weapon{
-		Name: "Gun",
-		Damage: 20,
+		Name:       "Gun",
+		Damage:     20,
 		Durability: 1.5,
 	}
 
 	w := &Gun
-	fmt.Printf("%s damages is -%d with durability of %.1f\n", w.Name, w.Damage, w.Durability)
+	fmt.Printf("%s damages is -%d with durability of %.1f before upgrade\n", w.Name, w.Damage, w.Durability)
+	Upgrade(w)
+	fmt.Printf("%s damages is -%d with durability of %.1f after upgrade\n", w.Name, w.Damage, w.Durability)
+
+	Mykel := Character{Name: "Mykel"}
+	var noName *Character = nil
+	c := &Mykel
+	BoostCharacter(c)
+	BoostCharacter(noName)
 }
